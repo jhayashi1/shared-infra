1edae6011e340d672352745f86fc1a026984555c
jhayashi1 <jaredhayashi@gmail.com>
2024-12-08 15:34:03 -0600

    fix: vpc syntax

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,22 +1,16 @@
 resource "aws_vpc" "ipv6_only_vpc" {
-  cidr_block       = "10.0.0.0/28"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  assign_generated_ipv6_cidr_block = true
+  enable_dns_support               = true
+  enable_dns_hostnames             = true
 
   tags = {
     Name = "ipv6-only"
   }
 }
 
-resource "aws_vpc_ipv6_cidr_block_association" "ipv6_cidr" {
-  vpc_id               = aws_vpc.ipv6_only_vpc.id
-  ipv6_ipam_pool_id    = null
-}
-
 resource "aws_subnet" "ipv6_only_subnet" {
-  vpc_id            = aws_vpc.ipv6_only_vpc.id
-  cidr_block        = null
-  ipv6_cidr_block   = cidrsubnet(aws_vpc_ipv6_cidr_block_association.ipv6_cidr.ipv6_cidr_block, 8, 0)
+  vpc_id                          = aws_vpc.ipv6_only_vpc.id
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.ipv6_only_vpc.ipv6_cidr_block, 8, 0)
   assign_ipv6_address_on_creation = true
 
   tags = {
