928adac0f5b419ce2f0e289a0731647635bd4a01
jhayashi1 <jaredhayashi@gmail.com>
2024-12-08 15:27:11 -0600

    fix: minimal cidr for vpc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,50 +1,51 @@
-resource "aws_vpc" "ipv6_vpc" {
-  cidr_block       = null
-  assign_generated_ipv6_cidr_block = true
-  enable_dns_support = true
+resource "aws_vpc" "ipv6_only_vpc" {
+  cidr_block       = "10.0.0.0/28"
   enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     Name = "ipv6-only"
   }
 }
 
-resource "aws_subnet" "ipv6_subnet" {
-  count                  = 2
-  vpc_id                 = aws_vpc.ipv6_vpc.id
-  ipv6_cidr_block        = cidrsubnet(aws_vpc.ipv6_vpc.ipv6_cidr_block, 8, count.index)
-  availability_zone      = data.aws_availability_zones.available.names[count.index]
+resource "aws_vpc_ipv6_cidr_block_association" "ipv6_cidr" {
+  vpc_id               = aws_vpc.ipv6_only_vpc.id
+  ipv6_ipam_pool_id    = null
+}
+
+resource "aws_subnet" "ipv6_only_subnet" {
+  vpc_id            = aws_vpc.ipv6_only_vpc.id
+  cidr_block        = null
+  ipv6_cidr_block   = cidrsubnet(aws_vpc_ipv6_cidr_block_association.ipv6_cidr.ipv6_cidr_block, 8, 0)
+  assign_ipv6_address_on_creation = true
 
   tags = {
-    Name = "ipv6-only-subnet-${count.index}"
+    Name = "ipv6-only-subnet"
   }
 }
 
-data "aws_availability_zones" "available" {}
-
-resource "aws_internet_gateway" "ipv6_igw" {
-  vpc_id = aws_vpc.ipv6_vpc.id
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.ipv6_only_vpc.id
 
   tags = {
-    Name = "ipv6-igw"
+    Name = "ipv6-only-igw"
   }
 }
 
 resource "aws_route_table" "ipv6_route_table" {
-  vpc_id = aws_vpc.ipv6_vpc.id
+  vpc_id = aws_vpc.ipv6_only_vpc.id
 
   route {
     ipv6_cidr_block = "::/0"
-    gateway_id      = aws_internet_gateway.ipv6_igw.id
+    gateway_id      = aws_internet_gateway.igw.id
   }
 
   tags = {
-    Name = "ipv6-route-table"
+    Name = "ipv6-only-route-table"
   }
 }
 
-resource "aws_route_table_association" "ipv6_rta" {
-  count          = length(aws_subnet.ipv6_subnet)
-  subnet_id      = aws_subnet.ipv6_subnet[count.index].id
+resource "aws_route_table_association" "ipv6_route_assoc" {
+  subnet_id      = aws_subnet.ipv6_only_subnet.id
   route_table_id = aws_route_table.ipv6_route_table.id
 }
